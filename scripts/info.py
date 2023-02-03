829af1004dca9b1645eb432330daaf3b43c2cc70
Rimi <59701116+AklimaRimi@users.noreply.github.com>
2023-02-03 22:16:42 +0600

    update

--- scripts/info.py
+++ scripts/info.py
@@ -3,48 +3,52 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import numpy as np
 
-driver = webdriver.Chrome()
-img_link = []
-data = []
-df = pd.read_csv('links.csv')
-pages = df['Link'].values.tolist()
+# driver = webdriver.Chrome()
+# img_link = []
+# data = []
+# df = pd.read_csv('links.csv')
+# pages = df['Link'].values.tolist()
 
-j=1
-for page in pages:
-    print(j)
-    j += 1
-    driver.get(page)
-    imgs = driver.find_elements(By.CLASS_NAME,"owl-stage")[0].find_elements(By.CLASS_NAME,'owl-item')
-    links=[]
-    for img in imgs:
-        image_link = img.find_element(By.TAG_NAME,'div').find_element(By.TAG_NAME,'a').get_attribute('href')
-        img_link.append(image_link)
+# j=1
+# for page in pages:
+#     print(j)
+#     j += 1
+#     driver.get(page)
+#     imgs = driver.find_elements(By.CLASS_NAME,"owl-stage")[0].find_elements(By.CLASS_NAME,'owl-item')
+#     links=[]
+#     for img in imgs:
+#         image_link = img.find_element(By.TAG_NAME,'div').find_element(By.TAG_NAME,'a').get_attribute('href')
+#         img_link.append(image_link)
         
-        a = image_link[::-1]
-        b = a.find('/') 
-        c = a[b+1:]
-        d = c.find('/')
-        e = a[:b+d]
-        f = e[::-1]
-        # print(f)
-        links.append(f)
-    name = driver.find_element(By.XPATH,'//h1[contains(@class,"h2")]').text
-    price = driver.find_element(By.XPATH,"//span[contains(@class,'new-price')]").text
-    description = driver.find_element(By.ID,'description').text
-    data.append([name,description,price,links])
-df = pd.DataFrame(data=data,columns = ['Product Name','Description','Price','Images'])
+#         a = image_link[::-1]
+#         b = a.find('/') 
+#         c = a[b+1:]
+#         d = c.find('/')
+#         e = a[:b+d]
+#         f = e[::-1]
+#         # print(f)
+#         links.append(f)
+#     name = driver.find_element(By.XPATH,'//h1[contains(@class,"h2")]').text
+#     price = driver.find_element(By.XPATH,"//span[contains(@class,'new-price')]").text
+#     description = driver.find_element(By.ID,'description').text
+#     lists=driver.find_element(By.ID,'detail-features').text
+#     description += '\n\n'+lists
+#     data.append([name,description,price,links])
+    
+# df = pd.DataFrame(data=data,columns = ['Product Name','Description','Price','Images'])
+# df.to_csv('info.csv',index=False)
+# imgdf = pd.DataFrame(data=img_link,columns=['link'])
+# imgdf.to_csv('img_link.csv',index=False)
 
-img = df['Images']
-def rep(data):
-    x = data.replace('[','')
+df = pd.read_csv('C:/pythons/Deep Learning/tasks/task2/ToyBoatProductScrapper/output/info.csv')
+def rep(x):
+    x = x.replace('[','')
     y = x.replace(']','')
     z = y.replace("'",'')
     return z
 df['Images'] = df['Images'].apply(lambda x: rep(x))
 print(df.head())
-df.to_csv('info.csv',index=False)
-imgdf = pd.DataFrame(data=img_link,columns=['link'])
-imgdf.to_csv('img_link.csv',index=False)
+df.to_csv('C:/pythons/Deep Learning/tasks/task2/ToyBoatProductScrapper/output/info.csv',index = False)
 # print(len(imgs))
 
 
